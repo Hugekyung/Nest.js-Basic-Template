a38eec9dfb24ed0de08b48dc3ec8d3df8ce6f8fb
haechan <haechan2000@noutecompany.com>
2023-08-04 18:49:08 +0900

    feat: eslint 각 옵션에 대한 설명 추가, prettier 옵션 추가, README 업데이트

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,24 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint/eslint-plugin'], // * Eslint에서 기본 제공하는 rules에 추가적인 규칙을 적용할 수 있게 플러그인을 등록
+    extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'], // * 외부에서 이미 정립된 Eslint 규칙 설정을 불러와 적용
+    root: true, // * 현재 디렉터리 단계에서 lint 파일을 찾으면 더 상위 디렉터리로 올라갈지 여부(true이면 처음으로 발견한 lint 파일의 단계에서 멈춤. 즉, 상위 폴더의 영향을 받지 않음)
+    // * env : 각 런타임 환경에 해당 설정 내용을 알려주는 설정
+    env: {
+        node: true,
+        jest: true,
+    },
+    ignorePatterns: ['.eslintrc.js'],
+    // * rules : 규칙 하나하나를 세세하게 제어하기 위한 것. extends에서 설정된 규칙 위에 덮어쓸 수 있음.
+    rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+    },
 };
--- README.md
+++ README.md
@@ -8,29 +8,47 @@ Nest.js 프레임워크 기반 Node 프로젝트 기본 템플릿
 
 ### `설정파일 세팅`
 
-- EsLint & Prettier 설정
-- Path Mapper 설정
-- tsconfig.json 설정
+-   EsLint & Prettier 설정
+    ([EsLint 설정 내용 참고](https://www.daleseo.com/eslint-config/))
+
+        ```bash
+          <Prettier>
+
+          {
+          "singleQuote": true,
+          "trailingComma": "all",
+          "tabWidth": 4, # 탭 너비
+          "printWidth": 108,
+          "editor.formatOnSave": true # 저장 시 format 변경
+          }
+        ```
+
+-   Path Mapper 설정
+-   tsconfig.json 설정
 
 ### `Nest.js 프레임워크 기능 세팅`
 
-- Interceptor - Custom Logger
-- Guard - JWT
-- ClassValidator
-- ClassTransformer
-- ValiationPipe
-- Custom Decorator
-- Exception Filter
-- Middleware
+-   Interceptor - Custom Logger
+-   Guard - JWT
+-   ClassValidator
+-   ClassTransformer
+-   ValiationPipe
+-   Custom Decorator
+-   Exception Filter
+-   Middleware
 
 ### `DataBase & 테스트 환경 세팅`
 
-- TypeORM(MySql)
-- Repository Patterns
-- Docker & Docker-compose
-- Sample Test Code with JEST
+-   TypeORM(MySql)
+-   Repository Patterns
+-   Docker & Docker-compose
+-   Sample Test Code with JEST
 
 ### `API`
 
-- REST & Swagger
-- GraphQL
+-   REST & Swagger
+-   GraphQL
+
+<br/>
+
+## Environments
