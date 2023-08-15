b97c66c6ea57fb7387395dded495d249310cb8dc
haechan <haechan2000@noutecompany.com>
2023-08-15 17:04:08 +0900

    feat: app.service 내 getUserInfo 추가, controller에 의존성 주입, return 코드 수정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { AppService } from 'src/app.service';
 
 @Controller('auth')
 export class AuthController {
+    constructor(private readonly appService: AppService) {}
+
     @Get()
     async getUserInfo() {
-        return { username: 'testUser' };
+        return this.appService.getUserInfo();
     }
 }
